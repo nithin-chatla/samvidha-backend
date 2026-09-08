568e0683d4c33a38ec3026816e2c920a49451366
nithin-chatla <nithinchatla0@gmail.com>
2026-09-08 23:53:24 +0530

    Fix FCM notification lag for anon chat

--- app.py
+++ app.py
@@ -2343,9 +2343,11 @@ def notify_anon_chat():
             message = message[:57] + "..."
         
         push_msg = messaging.Message(
+            notification=messaging.Notification(
+                title=title,
+                body=message,
+            ),
             data={
-                "title": title,
-                "body": message,
                 "route": "/anonymous_chat"
             },
             android=messaging.AndroidConfig(
